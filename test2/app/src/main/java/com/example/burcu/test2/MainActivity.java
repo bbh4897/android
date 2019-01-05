4c54aac1817519dffa32376199c729f71af51ed5
bbg <bagbrc@gmail.com>
2019-01-06 01:01:47 +0300

    Searc Projesi Tamamlandı

--- test2/app/src/main/java/com/example/burcu/test2/MainActivity.java
+++ test2/app/src/main/java/com/example/burcu/test2/MainActivity.java
@@ -1,33 +1,28 @@
 package com.example.burcu.test2;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WifiManager wifiManager;
-    private ListView listView;
-    private Button button;
-    private int size=0;
-    private List<ScanResult> results;
-    private ArrayList<String> arrayList = new ArrayList<>();
-    private ArrayAdapter adapter;
-
-
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    test t = new test();
 
 
     @Override
@@ -35,48 +30,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
+        listView = (ListView) findViewById(R.id.search);
+
+
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+        try {
+            arrayList.addAll(t.execute().get());
+
+            adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+            listView.setAdapter(adapter);
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+
+
             @Override
-            public void onClick(View v) {
-                    scanWifi();
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                if(listView.getVisibility() == View.VISIBLE){
+                    listView.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    listView.setVisibility(View.VISIBLE);
+                }
+
+                adapter.getFilter().filter(newText);
+                return false;
             }
         });
 
-
-        listView = findViewById(R.id.listView);
-        wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-
-        if(!wifiManager.isWifiEnabled()){
-            Toast.makeText(this,"wifiye baglan",Toast.LENGTH_LONG).show();
-            wifiManager.setWifiEnabled(true);
-        }
-
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
-        listView.setAdapter(adapter);
-        scanWifi();
+        return super.onCreateOptionsMenu(menu);
 
     }
 
-    private void scanWifi(){
-        arrayList.clear();
-        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        wifiManager.startScan();
-        Toast.makeText(this, "scan wifi", Toast.LENGTH_LONG).show();
 
-    }
-
-    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            results = wifiManager.getScanResults();
-            unregisterReceiver(this);
-
-
-            for(ScanResult scanResult :  results){
-                arrayList.add(scanResult.SSID + " - " + scanResult.capabilities + scanResult.level + scanResult.frequency);
-                adapter.notifyDataSetChanged();
-            }
-        }
-    };
 }
---
+++ test2/app/src/main/java/com/example/burcu/test2/test.java
@@ -0,0 +1,26 @@
+package com.example.burcu.test2;
+
+import android.os.AsyncTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test extends AsyncTask<String, String, ArrayList<String>> {
+
+
+    @Override
+    protected ArrayList<String> doInBackground(String... strings) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("jkljl");
+        list.add("nnnnn");
+        list.add("nnnnn");
+        list.add("nnnnn");
+        list.add("nnnnn");
+        list.add("nnnnn");
+        list.add("nnnnn");
+        list.add("oooo");
+
+        System.out.println("aaaaa : " + list);
+        return list;
+    }
+}

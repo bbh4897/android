ae15c0573220c440b2efdc2ab858f73fc9ba7cb3
bbg <bagbrc@gmail.com>
2019-02-22 17:02:04 +0300

    Ekleme ıslemının yapılacagı yenı bır actıvıty sayfası olusturuldu

---
+++ BPFirstlevel_v1/app/src/main/java/com/example/burcu/bpfirstlevel_v1/Activity_KonumBilgiGiris.java
@@ -0,0 +1,16 @@
+package com.example.burcu.bpfirstlevel_v1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Activity_KonumBilgiGiris extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__konum_bilgi_giris);
+
+
+    }
+}
--- BPFirstlevel_v1/app/src/main/java/com/example/burcu/bpfirstlevel_v1/MainActivity.java
+++ BPFirstlevel_v1/app/src/main/java/com/example/burcu/bpfirstlevel_v1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.burcu.bpfirstlevel_v1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,9 +30,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent q1 = new Intent(MainActivity.this, Activity_KonumBilgiGiris.class);
+                startActivity(q1);
 
             }
         });

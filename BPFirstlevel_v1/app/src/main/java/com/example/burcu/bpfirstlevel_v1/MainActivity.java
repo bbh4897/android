64a602024f79128944d2bc484d93119e2329a983
bbg <bagbrc@gmail.com>
2019-02-22 16:39:36 +0300

    Tema dizaynı yapıldı

--- BPFirstlevel_v1/app/src/main/java/com/example/burcu/bpfirstlevel_v1/MainActivity.java
+++ BPFirstlevel_v1/app/src/main/java/com/example/burcu/bpfirstlevel_v1/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);//        TOOLBAR OLARAK NITELENDIRILEN SEY, AYARLAR KISMI.
+        setSupportActionBar(toolbar); //   ACTION BAR'A TOOLBAR EKLEMESI YAPILDI
+
+
+
+//        ***************************************************************************************************************
+//        fLOATING ACTION BAR KISMINA TIKLANILDIGINDA NELER OLACAGI
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
             }
         });
     }

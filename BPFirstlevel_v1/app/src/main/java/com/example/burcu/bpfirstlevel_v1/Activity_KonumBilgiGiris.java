9b943e53552db50cbeb103f416a340a8eaae8573
bbg <bagbrc@gmail.com>
2019-02-22 19:55:09 +0300

    Konum Bilgi Girme Dizayn - İmageView'ı Telefon Galerısınden Secme

--- BPFirstlevel_v1/app/src/main/java/com/example/burcu/bpfirstlevel_v1/Activity_KonumBilgiGiris.java
+++ BPFirstlevel_v1/app/src/main/java/com/example/burcu/bpfirstlevel_v1/Activity_KonumBilgiGiris.java
@@ -1,5 +1,8 @@
 package com.example.burcu.bpfirstlevel_v1;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +18,9 @@ public class Activity_KonumBilgiGiris extends AppCompatActivity {
     EditText editText;
     Button btn;
     ImageView imImageView;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +30,41 @@ public class Activity_KonumBilgiGiris extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.editText);
         imImageView = (ImageView)findViewById(R.id.imageView);
         btn = (Button)findViewById(R.id.button);
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(editText.getText().toString().equals("aaas")){
-                    Snackbar.make(v, "sadasdasd", Snackbar.LENGTH_LONG)
-                       .setAction("Action", null).show();
-                }
-                else{
-                    Snackbar.make(v, "ssss", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
+//                if(editText.getText().toString().equals("aaas")){
+//                    Snackbar.make(v, "sadasdasd", Snackbar.LENGTH_LONG)
+//                       .setAction("Action", null).show();
+//                }
+//                else{
+//                    Snackbar.make(v, "ssss", Snackbar.LENGTH_LONG)
+//                            .setAction("Action", null).show();
+//                }
 
             }
         });
 
+        imImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent img_intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(img_intent, PICK_IMAGE);
+            }
+        });
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode== RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imImageView.setImageURI(imageUri);
+        }
     }
 }

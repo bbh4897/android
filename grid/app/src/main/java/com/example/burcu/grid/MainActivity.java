67663e273df6e2b29eb3e8e3f92f9b74fb308ae0
bbg <bagbrc@gmail.com>
2019-03-03 16:48:12 +0300

    Izgara resımsız tamam

--- grid/app/src/main/java/com/example/burcu/grid/MainActivity.java
+++ grid/app/src/main/java/com/example/burcu/grid/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.burcu.grid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
-}
+
+    public void btnIzgara(View view){
+
+        Button b = (Button)view;
+        String buttonText = b.getText().toString();
+            Toast.makeText(MainActivity.this, buttonText, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+
